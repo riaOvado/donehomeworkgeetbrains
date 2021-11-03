e1f75f43e06424d9e853a8986c431bfb96fe7a6b
ПК <d4704@mail.ru>
2021-11-03 17:37:57 +0300

    task2 done

--- src/main/java/org/example/homework3.java
+++ src/main/java/org/example/homework3.java
@@ -3,10 +3,12 @@ package org.example;
 public class homework3 {
     public static void main(String[] args) {
 taskOne();
+taskTwo();
     }
     public static void taskOne(){
         int [] a = {1,0,0,0,0,0,0,1};
         for (int i = 0; i < a.length; i++ ) {
+            System.out.println("TASK1");
             System.out.println("before");
             System.out.println(a[i]);
             if (a[i] == 0) {
@@ -18,5 +20,30 @@ taskOne();
             System.out.println(a[i]);
         }
     }
+
+    public static void taskTwo(){
+        System.out.println("TASK2");
+
+        int [] b = new int [100];
+        for(int i = 0; i<b.length;i++){
+            System.out.println(b[i]);
+            if (i<b.length){
+                b[i]=i + 1;
+                System.out.println(b[i]);
+            }
+
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
 }
 
