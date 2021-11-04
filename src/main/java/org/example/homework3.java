d84cf97e8408fb816dabf7253751e4baafffe31e
ПК <d4704@mail.ru>
2021-11-04 15:15:50 +0300

    task 3 done

--- src/main/java/org/example/homework3.java
+++ src/main/java/org/example/homework3.java
@@ -39,12 +39,13 @@ taskThree();
     }
 
     public static void taskThree(){
+        System.out.println("TASK3");
         int [] c = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         for (int i=0; i < c.length; i++ ) {
-            System.out.println(c[i]);
+
 
             if (c[i] < 6) {
-                c[i] = i * 2;
+                c[i] = c[i] * 2;
                 System.out.println(c[i]);
             }
         }
